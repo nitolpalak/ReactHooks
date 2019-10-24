fda6003e63f8a8a2454a01b8a1dc7c16e5e4c279
nitolpalak <nitolpalak@gmail.com>
2019-10-24 10:47:00 +0600

    Adding src/components folder

---
+++ src/components/HookCounter2.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function HookCounter2() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+      for(let i=0; i<5; i++){
+        //   setCount(count + 1)  //doesn't work... because this works on a different thread.. so the {count} is not previous State value
+          setCount((prevCount) => prevCount + 1)  //this works... beacause we pass the previous state value as function(arrow function in this case) to count new state value
+      }
+  }
+  return (
+    <div>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={incrementFive}>Increment 5</button>
+    </div>
+  );
+}
+
+export default HookCounter2;
