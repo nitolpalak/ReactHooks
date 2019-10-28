d583cdc67a6738c6e6f1ace9b50de0fb59d5e8d1
Mir Niaz Ahmed <mir.ahmed@bjitgroup.com>
2019-10-28 14:15:04 +0600

    Till Tutorial 53

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
-import HookCounter4 from './components/HookCounter4';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
+import HookCounter2 from "./components/HookCounter2";
+import HookCounter3 from "./components/HookCounter3";
+import HookCounter4 from "./components/HookCounter4";
+import ClassCounter1 from "./components/ClassCounter1";
+import HookCounter5 from "./components/HookCounter5";
+import HookCounter6 from "./components/HookCounter6";
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
     <div className="App">
-      <HookCounter4 />
+      <MouseContainer />
+      {/* <HookMouse /> */}
+      {/* <ClassMouse /> */}
+      {/* <HookCounter6 /> */}
+      {/* <ClassCounter1 /> */}
+      {/* <HookCounter5 /> */}
+      {/* <HookCounter4 /> */}
       {/* <HookCounter3 /> */}
       {/* <HookCounter2 /> */}
       {/* <ClassCounter />
---
+++ src/components/HookCounter6.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react";
+
+function HookCounter6() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log("Updating document title");
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+  return (
+    <div>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => setCount(count + 1)}>Click {count} times</button>
+    </div>
+  );
+}
+
+export default HookCounter6;
