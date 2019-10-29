e91e28e5f534226daf2676a74dd54027153035bc
Mir Niaz Ahmed <mir.ahmed@bjitgroup.com>
2019-10-29 13:54:16 +0600

    Till Tutorial 64 (own modification)

--- src/components/Counter3.js
+++ src/components/Counter3.js
@@ -2,11 +2,11 @@ import React, { useReducer } from "react";
 
 const initialState = 0;
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case "increment":
-      return state + 1;
+      return state + action.value;
     case "decrement":
-      return state - 1;
+      return state - action.value;
     case "reset":
       return initialState;
     default:
@@ -21,14 +21,14 @@ function Counter3() {
   return (
     <div>
       <h4>Count1 - {count}</h4>
-      <button onClick={() => dispatch("increment")}>Increment</button>
-      <button onClick={() => dispatch("decrement")}>Decrement</button>
-      <button onClick={() => dispatch("reset")}>Reset</button>
+      <button onClick={() => dispatch({type: "increment", value: 1})}>Increment</button>
+      <button onClick={() => dispatch({type: "decrement", value: 1})}>Decrement</button>
+      <button onClick={() => dispatch({type: "reset"})}>Reset</button>
       <div>
         <h4>Count2 - {count2}</h4>
-        <button onClick={() => dispatch2("increment")}>Increment</button>
-        <button onClick={() => dispatch2("decrement")}>Decrement</button>
-        <button onClick={() => dispatch2("reset")}>Reset</button>
+        <button onClick={() => dispatch2({type: "increment", value: 5})}>Increment</button>
+        <button onClick={() => dispatch2({type: "decrement", value:5})}>Decrement</button>
+        <button onClick={() => dispatch2({type: "reset"})}>Reset</button>
       </div>
     </div>
   );
